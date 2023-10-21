aa237e484d5b1d93fb6da2dc2e38137d436d8606
Jason Ramirez <12ramirezjason@gmail.com>
2023-10-21 11:03:02 -0700

    Final Report Date Oct 21, 2023

---
+++ mock_data.py
@@ -0,0 +1,60 @@
+
+
+catalog = [
+    {
+        "title": "Shoes",
+        "category": "Footwear",
+        "price": 39.99,
+        "image": "Shoes.jpeg",
+        "_id": "Thing1"
+    },
+    {
+        "title": "Hat",
+        "category": "Headwear",
+        "price": 9.99,
+        "image": "Hat.jpeg",
+        "_id": "Thing2"
+    },
+    {
+        "title": "Shirt",
+        "category": "Clothing",
+        "price": 19.99,
+        "image": "Japan.jpeg",
+        "_id": "Thing3"
+    },
+    {
+        "title": "Pants",
+        "category": "Clothing",
+        "price": 29.99,
+        "image": "Pants.jpeg",
+        "_id": "Thing4"
+    },
+    {
+        "title": "Socks",
+        "category": "Footwear",
+        "price": 5.99,
+        "image": "Socks.jpeg",
+        "_id": "Thing5"
+    },
+    {
+        "title": "Gloves",
+        "category": "Clothing",
+        "price": 9.99,
+        "image": "Gloves.jpeg",
+        "_id": "Thing6"
+    },
+    {
+        "title": "Scarf",
+        "category": "Clothing",
+        "price": 9.99,
+        "image": "Scarf.jpeg",
+        "_id": "Thing7"
+    },
+    {
+        "title": "Jacket",
+        "category": "Clothing",
+        "price": 49.99,
+        "image": "Jacket.jpeg",
+        "_id": "Thing8"
+    }
+]
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from config import me
+from mock_data import catalog
 import json
 
 app = Flask(__name__)
@@ -18,6 +19,8 @@ def anything():
 #########################
 ########### API##########
 #########################
+# TODO: create a catalog dictionary
+
 
 @app.get("/api/version")
 def version():
@@ -38,15 +41,42 @@ def about():
 # get /api/catalog
 # return the catalog dictionary as json
 @app.get("/api/catalog")
-def catalog():
+def get_catalog():
     return json.dumps(catalog)
 
 
 # get /api/products/
 # return the products dictionary as json
-@app.get("/api/products")
-def products():
-    return json.dumps(products)
+@app.post("/api/catalog")
+def save_products():
+    product = request.get_json()
+
+    product["_id"] = len(catalog)
+    catalog.append(product)
+
+    return json.dumps(product)
+
+# create a get /api/report/total
+# that sends the total value of your catalog(sum of all prices)
+
+
+@app.get("/api/report/total")
+def get_report_total():
+    total = 0
+    for prod in catalog:
+        total += prod["price"]
+    return json.dumps(total)
+
+
+# get all products for a given category
+@app.get("/api/products/<cat>")
+def get_products_by_category(cat):
+    # print(request.args)
+    results = []
+    for prod in catalog:
+        if (prod["category"] == cat):
+            results.append(prod)
+    return json.dumps(results)
 
 
 app.run(debug=True)
